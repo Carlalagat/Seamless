4981c0cdfd311b75e79eef9fd7e06b48773d4b49
kiki_glow <kinyaglory296@gmail.com>
2025-03-06 00:51:01 +0300

    added the tailor dashboard (including a sidebar it is in the figma UI)

--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import router from "./router";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faBars, faTimes, faChartLine, faBox, faRuler, faBullhorn, faBell, faUser, faSignOutAlt, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 /**styling */
 // import "./style.css";
@@ -11,9 +14,13 @@ import "./assets/tailwind.css"; //tailwind
 import { fakeBackend } from "./helpers";
 fakeBackend();
 
+// Add icons to library
+library.add(faBars, faTimes, faChartLine, faBox, faRuler, faBullhorn, faBell, faUser, faSignOutAlt, faSearch);
+
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
