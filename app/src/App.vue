91e31b581a491adab8331040b0b75917512df7d8
Ham Kemboi <kemboiham3@gmail.com>
2025-03-10 10:02:43 +0300

    measure

--- app/src/App.vue
+++ app/src/App.vue
@@ -12,6 +12,9 @@
 import { RouterView } from "vue-router";
 import Header from "./components/common/Header.vue";
 import Footer from "./components/common/Footer.vue";
+import MeasurementPage from "./pages/client/MeasurementPage.vue";
+// import Measurement from "./components/client/Measurement.vue";
+
 </script>
 
 <style scoped></style>
