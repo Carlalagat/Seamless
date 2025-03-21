c41cd0b11e4eb81d58e3591640d4fb083908bede
Manasehkim <kkim3929@gmail.com>
2025-03-21 12:49:18 +0300

    added preliminary links for posting and viewing garments

--- app/src/views/index.js
+++ app/src/views/index.js
@@ -12,6 +12,7 @@ import AdminView from "./admin/AdminView.vue";
 import ClientView from "./client/ClientView.vue";
 import TailorView from "./tailor/TailorView.vue";
 import MeasurementView from "./client/MeasurementView.vue";
+import productsView from "./tailor/productsView.vue";
 
 export {
   HomeView,
@@ -28,4 +29,5 @@ export {
   ClientView,
   TailorView,
   MeasurementView,
+  productsView,
 };
