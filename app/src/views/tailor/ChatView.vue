f358544ada33919e55d1e91fa655713807e6da28
unknown <shemkangogo@gmail.com>
2025-03-25 11:48:07 +0300

    fixed ChatView page in views

---
+++ app/src/views/tailor/ChatView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <ChatPage />
+    </div>
+  </template>
+  
+  <script setup>
+  import ChatPage from "../../pages/tailor/ChatPage.vue";
+  </script>
+  
+  <style lang="scss" scoped></style>
+  
