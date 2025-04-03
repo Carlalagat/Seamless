23b9ae98b59c7e7d29a9dcf2b44a60c7f36d6d12
DonArtkins <info.sycx.ke@gmail.com>
2025-04-03 13:06:46 +0300

    conditionally displaying nav bar and footer

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="min-h-screen bg-white">
-    <Header />
+    <Header v-if="isPublicRoute" />
     <div class="mt-4">
       <RouterView />
     </div>
-    <Footer />
+    <Footer v-if="isPublicRoute" />
   </div>
 </template>
 
 <script setup>
-import { RouterView } from "vue-router";
+import { RouterView, useRoute, useRouter } from "vue-router";
+import { computed } from "vue";
 import Header from "./components/common/Header.vue";
 import Footer from "./components/common/Footer.vue";
 
+// List of public pages (matching the same list in your router)
+const publicPages = [
+  "/",
+  "/about",
+  "/contact",
+  "/find-garments",
+  "/find-tailors",
+  "/measurements",
+];
+
+const route = useRoute();
+
+// Computed property to check if the current route is public
+const isPublicRoute = computed(() => {
+  return publicPages.includes(route.path);
+});
 </script>
 
 <style scoped></style>
